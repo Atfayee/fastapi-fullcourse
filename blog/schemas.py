793fda69d394c99ae33bc91147ba34dbf6cf8930
Atfayee <jingchi.zhang@outlook.com>
2024-03-24 17:58:45 -0400

    modify user response

--- blog/schemas.py
+++ blog/schemas.py
@@ -14,4 +14,10 @@ class User(BaseModel):
     name:str
     email:str
     password:str 
+
+class ShowUser(BaseModel):
+    name:str
+    email:str
+    class Config():
+        from_attributes = True
     
