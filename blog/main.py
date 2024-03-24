e1841a649c81f0a8ae7ff71383d30afd7c131692
Atfayee <jingchi.zhang@outlook.com>
2024-03-24 15:36:31 -0400

    create database connection

--- blog/main.py
+++ blog/main.py
@@ -1,16 +1,10 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from . import schemas
 
 # uvicorn blog.main:app --reload
 app = FastAPI()
 
 
-
-class Blog(BaseModel):
-    title:str
-    body:str
-
-
 @app.post('/blog')
-def create(request: Blog):
+def create(request: schemas.Blog):
     return request
