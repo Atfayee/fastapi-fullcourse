146395faa88ce3ee1cf4e42d9994d52a000b766f
Atfayee <jingchi.zhang@outlook.com>
2024-03-25 14:14:28 -0400

    foreigin key

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,23 +1,37 @@
+from typing import List
 from pydantic import BaseModel
 
-class Blog(BaseModel):
-    title:str
-    body:str
 
-class ShowBlog(BaseModel):
-    title:str
-    body:str
-    class Config():
-        from_attributes = True
 
 class User(BaseModel):
     name:str
     email:str
     password:str 
 
+class BlogBase(BaseModel):
+    title:str
+    body:str
+
+    
+class Blog(BlogBase):
+    class Config():
+        from_attributes = True
+
+
+    
+
 class ShowUser(BaseModel):
     name:str
     email:str
+    blogs: List[Blog] = []
     class Config():
         from_attributes = True
     
+
+
+class ShowBlog(BaseModel):
+    title:str
+    body:str
+    creator: ShowUser
+    class Config():
+        from_attributes = True
